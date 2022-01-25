fd01db298d3c21c37d0e678fb8b4f7d7824613af
Darcy Raimond <darcy.raimond@gmail.com>
2022-01-25 19:55:25 +1100

    changed setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="d_tennis",
+    version="0.0.1",
+    author="Darcy Raimond",
+    author_email="darcy.raimond@gmail.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+)
+
+
+"""project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },"""
+"""classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],"""
