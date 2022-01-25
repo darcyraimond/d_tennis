066a4d46010944cbe5ed408f4acb8025e6dd935b
Darcy Raimond <darcy.raimond@gmail.com>
2022-01-25 20:19:33 +1100

    changed setup

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     url="",
 
     package_dir={"": "src"},
-    packages=setuptools.find_packages(),
+    packages=["src"]#setuptools.find_packages(where="src"),
     python_requires=">=3.8",
 )
 
