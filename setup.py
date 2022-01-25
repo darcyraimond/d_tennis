6b3f2c8c771e1c3877ffbd124890b0b8c726a54b
Darcy Raimond <darcy.raimond@gmail.com>
2022-01-25 20:21:04 +1100

    changed setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dTennis",
+    name="d_tennis",
     version="0.0.1",
     author="Darcy Raimond",
     author_email="darcy.raimond@gmail.com",
